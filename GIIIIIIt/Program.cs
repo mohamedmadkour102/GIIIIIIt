826be90b9c7901aec67d5fbde6b5ea061b35ae33
Mohamed Madkour <Mohamed.Madkour2002@gmail.com>
2024-07-27 02:13:51 +0300

    Question 1

--- GIIIIIIt/Program.cs
+++ GIIIIIIt/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //Q1
+            Console.WriteLine("Hello, Q1");
+
         }
     }
 }
